9aa6374257f59aee1ba3837ab31387493e6dc234
Marco Reveiz <revvace@estudiantec.cr>
2022-12-19 11:53:50 -0600

    FrontEnd Login

--- angular-electron/src/app/app.module.ts
+++ angular-electron/src/app/app.module.ts
@@ -4,9 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 // Angular Material
 import {MatFormFieldModule} from '@angular/material/form-field';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
--- angular-electron/src/app/components/login/login.component.ts
+++ angular-electron/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  form: FormGroup
+  constructor( private fb: FormBuilder){
 
+    this.form = this.fb.group({
+      user : ['',Validators.required],
+      password : ['',Validators.required]
+
+    })
+
+  }
+
+  ngOnInit() : void{
+
+  }
+
+  ingresar(){
+    console.log(this.form);
+    const user = this.form.value.user;
+    const password = this.form.value.password;
+  }
 }
