a055efaed121282444eaeb618bc711a0f6c7b70e
Steven Granados <svengra01@gmail.com>
2022-12-28 14:21:41 -0600

    100% ready Main Branch

--- angular-electron/src/app/components/dashboard/charges/charges.component.ts
+++ angular-electron/src/app/components/dashboard/charges/charges.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ProductoService } from 'src/app/services/producto.service';
-import { VentaService } from 'src/app/services/venta.service';
 import { Product } from '../product/product.component';
 import { ThemePalette } from '@angular/material/core';
 import { lastValueFrom } from 'rxjs';
 import { MatTableDataSource } from '@angular/material/table';
-import { ThisReceiver } from '@angular/compiler';
+import { VentaService } from 'src/app/services/venta.service';
+
 
 
 @Component({
@@ -16,8 +16,6 @@ import { ThisReceiver } from '@angular/compiler';
   styleUrls: ['./charges.component.css']
 })
 export class ChargesComponent {
-  carrito: Product[] = [];
-  
   // Formulario de Busqueda
   formBuscar: FormGroup
   // Formulario de Venta
@@ -66,66 +64,21 @@ export class ChargesComponent {
   ngOnInit() {}
   venta(){}
 
-  async getVentaByDate(fecha){
-    const data$ = this.ventaService.getVentaByDate(fecha);
-    const data = await lastValueFrom(data$);
-    this.venta = data[0];
-  }
 
-  async getVenta(IDVenta){
-    const data$ = this.ventaService.getVenta(IDVenta);
-    const data = await lastValueFrom(data$);
-    this.venta = data[0];
-  }
-    
-  async updateVenta(){
-    const data$ = this.ventaService.updateVenta(this.venta);
-    const data = await lastValueFrom(data$);
-    
-  }
-
-  async deleteVenta(IDVenta){
-    const data$ = this.ventaService.deleteVenta(IDVenta);
-    const data = await lastValueFrom(data$);
-
-  }
-  async addVenta(){
-    const data$ = this.ventaService.addVenta(this.venta);
-    const data = await lastValueFrom(data$);
-  }
-  
-  /*
-  async getAllVentas() {
-    const data$ = this.ventaService.getAllVentas(); 
-    this.ventas = data;
-    const data = await lastValueFrom(data$);
-  }
-  */
-
-  /*
-  async updateProducto(){
-    const data$ = this.productService.updateProducto(this.product);
-    const data = await lastValueFrom(data$);
-  }
-
-  async getProductoByID(IDProducto){
-    const data$ = this.productService.getProducto(IDProducto);
-    const data = await lastValueFrom(data$);
-    this.product = data[0];
-  }
-  */
 
   async buscarProducto(){
     // Obtenemos valores del formularioS
     const name = this.formBuscar.value.nameProduct;
     const barCode = this.formBuscar.value.codeProduct;
-    
-    // Asignamos valores al objeto producto
     const quantity = this.formBuscar.value.units;
+    // Asignamos valores al objeto producto
+    this.product.cantidad = quantity;
     this.product.codigoBarra = barCode;
     this.product.nombre = name;
     this.product.IDVenta = 'NULL';
     // consulta SQL
+
+  
 
     if (barCode){
       await this.getProductoBarras(barCode);    
@@ -142,21 +95,23 @@ export class ChargesComponent {
           verticalPosition: 'bottom'
         })      
     }
-
-}
-    async getProductoBarras(codigoBarra){
-      const data$ = this.ventaService.getProductoBarras(codigoBarra);
-      const data = await lastValueFrom(data$);
-      this.products = data;
-    }
-  
-
-    async getProductoNombre(nombre){
     
-      const data$ = this.ventaService.getProductoNombre(nombre);
-      const data = await lastValueFrom(data$);
-      this.products = data;
-    }
+    await this.setElementDataSearch();
+    
+  }
+
+  async getProductoBarras(codigoBarra){
+    const data$ = this.ventaService.getProductoBarras(codigoBarra);
+    const data = await lastValueFrom(data$);
+    this.products = data;
+  }
+
+  async getProductoNombre(nombre){
+    
+    const data$ = this.ventaService.getProductoNombre(nombre);
+    const data = await lastValueFrom(data$);
+    this.products = data;
+  }
 
   async setElementDataSearch(){
     let tempData: Product[] = [];
