a2249573eb335d83bd4824113eef6ef9ac799d99
Marco Reveiz <revvace@estudiantec.cr>
2022-12-30 19:03:34 -0600

    Generate PDF

--- angular-electron/src/app/components/dashboard/charges/charges.component.ts
+++ angular-electron/src/app/components/dashboard/charges/charges.component.ts
@@ -8,6 +8,9 @@ import { ThemePalette } from '@angular/material/core';
 import { lastValueFrom } from 'rxjs';
 import { MatTableDataSource } from '@angular/material/table';
 import { ThisReceiver } from '@angular/compiler';
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 
 @Component({
@@ -197,6 +200,29 @@ export class ChargesComponent {
     this.setElementData();
   }
 
+  createPDF(){
+    const pdfDefinition: any  = {
+      content: [
+        {
+          layout: 'lightHorizontalLines', // optional
+          table: {
+            // headers are automatically repeated if the table spans over multiple pages
+            // you can declare how many rows should be treated as headers
+            headerRows: 1,
+            widths: [ '*', 'auto', 100, '*' ],
+    
+            body: [
+              [ 'First', 'Second', 'Third', 'The last one' ],
+              [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
+              [ { text: 'Bold value', bold: true }, 'Val 2', 'Val 3', 'Val 4' ]
+            ]
+          }
+        }
+      ]
+    };
+
+    const pdf = pdfMake.createPdf(pdfDefinition).download();
+  }
   async agregarVenta(){
     this.venta.IDInventario = 2;
     this.venta.cantidad = 0;
@@ -213,6 +239,7 @@ export class ChargesComponent {
     this.venta.monto = this.formVenta.value.total;
     await this.addVenta();
     this.resetPage();
+    this.createPDF();
   }
 
   setProductOnNull(){
@@ -265,5 +292,7 @@ export class ChargesComponent {
     }
     await this.setElementDataSearch();
   }
+
+  
 
 }
